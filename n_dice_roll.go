82ebddbfadbe8c91cff27ed241ecc858b6ed87ca
a-temlyakov <temlyaka@gmail.com>
2014-12-11 21:43:11 -0800

    Refactored for loop and ran  on both files

--- n_dice_roll.go
+++ n_dice_roll.go
@@ -1,18 +1,18 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
 func n_dice_roll(n_sides int, n_rolls int) int {
 	my_dice := Dice{n_sides: n_sides}
-        my_sum := 0
-        i := 0 
-	for i < n_rolls{
+	my_sum := 0
+	for i := 0; i < n_rolls; i++ {
 		my_sum += my_dice.Roll()
-		i += 1
 	}
 	return my_sum
 }
 
-func main(){
+func main() {
 	fmt.Println(n_dice_roll(6, 2))
 }
