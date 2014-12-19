0268a643509e5297c106e98e1fe1f9deca831ce8
Kirill <Akavall@gmail.com>
2014-12-18 22:14:37 -0600

    applied gofmt tool and renamed the test file

--- calc_roll_probabilities.go
+++ calc_roll_probabilities.go
@@ -1,9 +1,8 @@
 package main
 
-
 func CalcRollPermutations(score int, n_dice int, n_sides int) int {
 	pc := PermCalculator{count: 0}
-	return pc.calc_combs(n_dice, 0, n_sides, 0, score)	
+	return pc.calc_combs(n_dice, 0, n_sides, 0, score)
 }
 
 type PermCalculator struct {
@@ -11,16 +10,16 @@ type PermCalculator struct {
 }
 
 func (pc *PermCalculator) calc_combs(n_dice int, score int, n_sides int, roll int, target int) int {
-	if (n_dice == 1){
-		if ( target-score >= 1 && target-score <= n_sides){
+	if n_dice == 1 {
+		if target-score >= 1 && target-score <= n_sides {
 			pc.count++
 			return 1
 		} else {
-			return 0 
+			return 0
 		}
 	}
 	for i := 1; i <= n_sides; i++ {
-		pc.calc_combs(n_dice - 1, score + i, n_sides, i, target)
+		pc.calc_combs(n_dice-1, score+i, n_sides, i, target)
 	}
 	return pc.count
 }
--- calc_roll_probabilities_test.go
+++ calc_roll_probabilities_test.go
@@ -1,21 +1,22 @@
 package main
 
-import ( "testing"
+import (
+	"testing"
 )
 
 func TestCalcRollPermutations_TwoDice_SixSides(t *testing.T) {
 
 	const N_DICE, N_SIDES = 2, 6
 
-	expected_score_to_n_pos := make(map[int] int)
-	
- 	for i := 1; i <= N_SIDES; i++ {
+	expected_score_to_n_pos := make(map[int]int)
+
+	for i := 1; i <= N_SIDES; i++ {
 		for j := 1; j <= N_SIDES; j++ {
-			expected_score_to_n_pos[i + j] += 1
+			expected_score_to_n_pos[i+j] += 1
 		}
 	}
 
-	for i := N_SIDES; i <= N_DICE * N_SIDES; i++ {
+	for i := N_SIDES; i <= N_DICE*N_SIDES; i++ {
 		result := CalcRollPermutations(i, N_DICE, N_SIDES)
 		expected := expected_score_to_n_pos[i]
 		if result != expected {
@@ -28,17 +29,17 @@ func TestCalcRollPermutations_ThreeDice_SevenSides(t *testing.T) {
 
 	const N_DICE, N_SIDES = 3, 7
 
-	expected_score_to_n_pos := make(map[int] int)
-	
- 	for i := 1; i <= N_SIDES; i++ {
+	expected_score_to_n_pos := make(map[int]int)
+
+	for i := 1; i <= N_SIDES; i++ {
 		for j := 1; j <= N_SIDES; j++ {
 			for k := 1; k <= N_SIDES; k++ {
-				expected_score_to_n_pos[i + j + k] += 1
+				expected_score_to_n_pos[i+j+k] += 1
 			}
 		}
 	}
 
-	for i := N_SIDES; i <= N_DICE * N_SIDES; i++ {
+	for i := N_SIDES; i <= N_DICE*N_SIDES; i++ {
 		result := CalcRollPermutations(i, N_DICE, N_SIDES)
 		expected := expected_score_to_n_pos[i]
 		if result != expected {
@@ -51,21 +52,21 @@ func TestCalcRollPermutations_FiveDice_TenSides(t *testing.T) {
 
 	const N_DICE, N_SIDES = 5, 10
 
-	expected_score_to_n_pos := make(map[int] int)
-	
- 	for i := 1; i <= N_SIDES; i++ {
+	expected_score_to_n_pos := make(map[int]int)
+
+	for i := 1; i <= N_SIDES; i++ {
 		for j := 1; j <= N_SIDES; j++ {
 			for k := 1; k <= N_SIDES; k++ {
-				for l := 1; l <= N_SIDES; l++{
-					for m := 1; m <= N_SIDES; m++{
-						expected_score_to_n_pos[i + j + k + l + m] += 1
+				for l := 1; l <= N_SIDES; l++ {
+					for m := 1; m <= N_SIDES; m++ {
+						expected_score_to_n_pos[i+j+k+l+m] += 1
 					}
 				}
 			}
 		}
 	}
 
-	for i := N_SIDES; i <= N_DICE * N_SIDES; i++ {
+	for i := N_SIDES; i <= N_DICE*N_SIDES; i++ {
 		result := CalcRollPermutations(i, N_DICE, N_SIDES)
 		expected := expected_score_to_n_pos[i]
 		if result != expected {
@@ -73,5 +74,3 @@ func TestCalcRollPermutations_FiveDice_TenSides(t *testing.T) {
 		}
 	}
 }
-
-
