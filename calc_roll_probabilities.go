98319df7c3735f9ab073393ee52d0f359315eae5
Kirill <Akavall@gmail.com>
2014-12-18 22:09:26 -0600

    renamed class to refer to Permutations

--- calc_roll_probabilities.go
+++ calc_roll_probabilities.go
@@ -2,25 +2,25 @@ package main
 
 
 func CalcRollPermutations(score int, n_dice int, n_sides int) int {
-	cc := CombCalculator{count: 0}
-	return cc.calc_combs(n_dice, 0, n_sides, 0, score)	
+	pc := PermCalculator{count: 0}
+	return pc.calc_combs(n_dice, 0, n_sides, 0, score)	
 }
 
-type CombCalculator struct {
+type PermCalculator struct {
 	count int
 }
 
-func (cc *CombCalculator) calc_combs(n_dice int, score int, n_sides int, roll int, target int) int {
+func (pc *PermCalculator) calc_combs(n_dice int, score int, n_sides int, roll int, target int) int {
 	if (n_dice == 1){
 		if ( target-score >= 1 && target-score <= n_sides){
-			cc.count++
+			pc.count++
 			return 1
 		} else {
 			return 0 
 		}
 	}
 	for i := 1; i <= n_sides; i++ {
-		cc.calc_combs(n_dice - 1, score + i, n_sides, i, target)
+		pc.calc_combs(n_dice - 1, score + i, n_sides, i, target)
 	}
-	return cc.count
+	return pc.count
 }
