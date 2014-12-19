5c9ee3bd08f98c2e5d8c171258a7bd258a4567a1
Kirill <Akavall@gmail.com>
2014-12-18 21:59:40 -0600

    wrote function that calculates number of possible combinations for a given score

---
+++ calc_roll_probabilities.go
@@ -0,0 +1,26 @@
+package main
+
+
+func CalcRollProbabilities(score int, n_dice int, n_sides int) int {
+	cc := CombCalculator{count: 0}
+	return cc.calc_combs(n_dice, 0, n_sides, 0, score)	
+}
+
+type CombCalculator struct {
+	count int
+}
+
+func (cc *CombCalculator) calc_combs(n_dice int, score int, n_sides int, roll int, target int) int {
+	if (n_dice == 1){
+		if ( target-score >= 1 && target-score <= n_sides){
+			cc.count++
+			return 1
+		} else {
+			return 0 
+		}
+	}
+	for i := 1; i <= n_sides; i++ {
+		cc.calc_combs(n_dice - 1, score + i, n_sides, i, target)
+	}
+	return cc.count
+}
---
+++ roll_probabilities_test.go
@@ -0,0 +1,77 @@
+package main
+
+import ( "testing"
+)
+
+func TestCalcRollProbabilities_TwoDice_SixSides(t *testing.T) {
+
+	const N_DICE, N_SIDES = 2, 6
+
+	expected_score_to_n_pos := make(map[int] int)
+	
+ 	for i := 1; i <= N_SIDES; i++ {
+		for j := 1; j <= N_SIDES; j++ {
+			expected_score_to_n_pos[i + j] += 1
+		}
+	}
+
+	for i := N_SIDES; i <= N_DICE * N_SIDES; i++ {
+		result := CalcRollProbabilities(i, N_DICE, N_SIDES)
+		expected := expected_score_to_n_pos[i]
+		if result != expected {
+			t.Errorf("For score %d with %d dice and %d sides expected %d but got %d", i, N_DICE, N_SIDES, expected, result)
+		}
+	}
+}
+
+func TestCalcRollProbabilities_ThreeDice_SevenSides(t *testing.T) {
+
+	const N_DICE, N_SIDES = 3, 7
+
+	expected_score_to_n_pos := make(map[int] int)
+	
+ 	for i := 1; i <= N_SIDES; i++ {
+		for j := 1; j <= N_SIDES; j++ {
+			for k := 1; k <= N_SIDES; k++ {
+				expected_score_to_n_pos[i + j + k] += 1
+			}
+		}
+	}
+
+	for i := N_SIDES; i <= N_DICE * N_SIDES; i++ {
+		result := CalcRollProbabilities(i, N_DICE, N_SIDES)
+		expected := expected_score_to_n_pos[i]
+		if result != expected {
+			t.Errorf("For score %d with %d dice and %d sides expected %d but got %d", i, N_DICE, N_SIDES, expected, result)
+		}
+	}
+}
+
+func TestCalcRollProbabilities_FiveDice_TenSides(t *testing.T) {
+
+	const N_DICE, N_SIDES = 5, 10
+
+	expected_score_to_n_pos := make(map[int] int)
+	
+ 	for i := 1; i <= N_SIDES; i++ {
+		for j := 1; j <= N_SIDES; j++ {
+			for k := 1; k <= N_SIDES; k++ {
+				for l := 1; l <= N_SIDES; l++{
+					for m := 1; m <= N_SIDES; m++{
+						expected_score_to_n_pos[i + j + k + l + m] += 1
+					}
+				}
+			}
+		}
+	}
+
+	for i := N_SIDES; i <= N_DICE * N_SIDES; i++ {
+		result := CalcRollProbabilities(i, N_DICE, N_SIDES)
+		expected := expected_score_to_n_pos[i]
+		if result != expected {
+			t.Errorf("For score %d with %d dice and %d sides expected %d but got %d", i, N_DICE, N_SIDES, expected, result)
+		}
+	}
+}
+
+
