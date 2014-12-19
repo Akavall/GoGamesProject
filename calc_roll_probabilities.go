23f0ab556640f7399be29c223d1e6371d1d8a147
Kirill <Akavall@gmail.com>
2014-12-18 22:18:25 -0600

    changed calc_combs to calc perms

--- calc_roll_probabilities.go
+++ calc_roll_probabilities.go
@@ -1,25 +1,26 @@
 package main
 
+
 func CalcRollPermutations(score int, n_dice int, n_sides int) int {
 	pc := PermCalculator{count: 0}
-	return pc.calc_combs(n_dice, 0, n_sides, 0, score)
+	return pc.calc_combs(n_dice, 0, n_sides, 0, score)	
 }
 
 type PermCalculator struct {
 	count int
 }
 
-func (pc *PermCalculator) calc_combs(n_dice int, score int, n_sides int, roll int, target int) int {
-	if n_dice == 1 {
-		if target-score >= 1 && target-score <= n_sides {
+func (pc *PermCalculator) calc_perms(n_dice int, score int, n_sides int, roll int, target int) int {
+	if (n_dice == 1){
+		if ( target-score >= 1 && target-score <= n_sides){
 			pc.count++
 			return 1
 		} else {
-			return 0
+			return 0 
 		}
 	}
 	for i := 1; i <= n_sides; i++ {
-		pc.calc_combs(n_dice-1, score+i, n_sides, i, target)
+		pc.calc_perms(n_dice - 1, score + i, n_sides, i, target)
 	}
 	return pc.count
 }
