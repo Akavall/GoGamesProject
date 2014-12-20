e42b5eae386ab4f9cd473564555c5c4891e5b03f
Kirill <Akavall@gmail.com>
2014-12-19 22:32:41 -0600

    added a way to show probabilitiesof 4 standard die rolls

--- basic_server.go
+++ basic_server.go
@@ -23,9 +23,28 @@ func dice_roll(response http.ResponseWriter, request *http.Request) {
     fmt.Fprintf(response, "%d", side.numerical_value)
 }
 
+func four_dice_roll(response http.ResponseWriter, request *http.Request) {
+    response.Header().Set("Content-type", "text/plain")
+
+    // Parse URL and POST data into the request.Form
+    err := request.ParseForm()
+    if err != nil {
+        log.Fatal(response, fmt.Sprintf("error parsing url %v", err), 500)
+    }
+
+    score := n_dice_roll(6, 4)
+    roll_prob, prob_lower, prob_higher := CalcRollProbabilities(score, 4, 6)
+   
+    log.Printf("Rolled: %d for request...", score)
+
+    // Actual response sent to web client
+    fmt.Fprintf(response, " score : %d\n roll prob : %f\n prob lower : %f\n prob higher : %f\n", score, roll_prob, prob_lower, prob_higher)
+}
+
+
 func main() {
     mux := http.NewServeMux()
-    mux.HandleFunc("/", dice_roll)
+    mux.HandleFunc("/", four_dice_roll)
     log.Printf("Started dumb Dice web server! Try it on http://localhost:8000")
     err := http.ListenAndServe(":8000", mux)
     
