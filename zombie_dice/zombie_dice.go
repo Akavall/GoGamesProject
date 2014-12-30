5c10afabee157889e11f82554f0e03b855e7d83c
Kirill Temlyakov <Kirill@simplerelevance.com>
2014-12-30 00:32:12 -0600

    formatted with gofmt

--- zombie_dice/zombie_dice.go
+++ zombie_dice/zombie_dice.go
@@ -1,30 +1,30 @@
 package zombie_dice
 
 import (
-	"fmt"
-	"time"
-	"math/rand"
 	"bufio"
+	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
-        "github.com/Akavall/GoGamesProject/dice"
+	"github.com/Akavall/GoGamesProject/dice"
 )
 
-func initialize_deck () []dice.Dice {
+func initialize_deck() []dice.Dice {
 
 	green := []string{"shot", "walk", "walk", "brain", "brain", "brain"}
 	yellow := []string{"shot", "shot", "walk", "walk", "brain", "brain"}
 	red := []string{"shot", "shot", "shot", "walk", "walk", "brain"}
-        
+
 	green_sides := make_slice_of_sides(green)
 	yellow_sides := make_slice_of_sides(yellow)
-        red_sides := make_slice_of_sides(red)
-	
-        // Put dices in the deck
+	red_sides := make_slice_of_sides(red)
 
-        const N_GREEN, N_YELLOW, N_RED = 6, 4, 3
+	// Put dices in the deck
+
+	const N_GREEN, N_YELLOW, N_RED = 6, 4, 3
 
 	deck := make([]dice.Dice, 0)
 
@@ -39,7 +39,7 @@ func initialize_deck () []dice.Dice {
 	for i := 0; i < N_RED; i++ {
 		deck = append(deck, dice.Dice{Name: "red", Sides: red_sides})
 	}
-	
+
 	return deck
 }
 
@@ -65,35 +65,35 @@ func make_slice_of_sides(string_sides []string) []dice.Side {
 func players_turn(deck []dice.Dice, ai bool) int {
 
 	brains := 0
-        shots := 0
+	shots := 0
 	old_dices := make([]dice.Dice, 0)
-        reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
 	// While loop
 	for i := 0; i < 1; i += 0 {
-		if (len(deck) + len(old_dices) < 3) {
+		if len(deck)+len(old_dices) < 3 {
 			fmt.Println("You have ran out of dices")
-                        fmt.Printf("Your final score is : %d", brains)
+			fmt.Printf("Your final score is : %d", brains)
 			return brains
 		}
-		dices_to_roll := pop_last_n(&deck, 3 - len(old_dices))
+		dices_to_roll := pop_last_n(&deck, 3-len(old_dices))
 		dices_to_roll = append(dices_to_roll, old_dices...)
 		old_dices = nil
 		for _, d := range dices_to_roll {
 			inner_walks := 0
 			side := d.Roll()
 			fmt.Println("You Rolled : ", d.Name, side.Name)
-			if (side.Name == "brain") {
+			if side.Name == "brain" {
 				brains++
-			} else if (side.Name == "shot") {
+			} else if side.Name == "shot" {
 				shots++
 			} else {
 				inner_walks++
-			old_dices = append(old_dices, d)
+				old_dices = append(old_dices, d)
 			}
 		}
 
-		if (shots >= 3) {
+		if shots >= 3 {
 			fmt.Println("You have been shot 3 times, you've scored 0")
 			return 0
 		}
@@ -104,23 +104,23 @@ func players_turn(deck []dice.Dice, ai bool) int {
 
 		var answer int
 
-		if (ai == false) {
+		if ai == false {
 			raw_string, _ := reader.ReadString('\n')
 			clean_string := strings.Replace(raw_string, "\n", "", -1)
 			answer, _ = strconv.Atoi(clean_string)
 		} else {
-			if (shots == 2) {
+			if shots == 2 {
 				answer = 0
 			} else {
 				answer = 1
 			}
 		}
 
-		if (answer == 0) {
+		if answer == 0 {
 			fmt.Println("You scored : ", brains)
 			return brains
 		}
-		
+
 		// emptying the slice
 	}
 	fmt.Println("The turn has ended")
@@ -130,10 +130,10 @@ func players_turn(deck []dice.Dice, ai bool) int {
 func pop_last_n(a_ptr *[]dice.Dice, n_to_pop int) []dice.Dice {
 
 	a := *a_ptr
-	poped_slice := a[len(a) - n_to_pop : len(a)]
-	a = append(a[:0], a[:len(a) - n_to_pop]...)
-        *a_ptr = a
-        
+	poped_slice := a[len(a)-n_to_pop : len(a)]
+	a = append(a[:0], a[:len(a)-n_to_pop]...)
+	*a_ptr = a
+
 	return poped_slice
 }
 
@@ -153,22 +153,21 @@ func PlayWithAI() {
 		ai_score := players_turn(shuffled_deck, true)
 		ai_total_score += ai_score
 
-                fmt.Printf("Round : %d\n", i)
+		fmt.Printf("Round : %d\n", i)
 		fmt.Printf("Your total score is : %d\n", player_total_score)
 		fmt.Printf("AI total score is : %d\n", ai_total_score)
 
-		if ( player_total_score  >= 13 || ai_total_score >= 13 ){
-			if (player_total_score > ai_total_score) {
+		if player_total_score >= 13 || ai_total_score >= 13 {
+			if player_total_score > ai_total_score {
 				fmt.Println("Congratulations You Won!")
 				return
-			} else if (player_total_score < ai_total_score) {
+			} else if player_total_score < ai_total_score {
 				fmt.Println("AI won! Better Luck Next Time!")
 				return
 			}
 		}
 	}
 }
-
 
 // func main() {
 // 	play_with_ai()
--- zombie_dice/zombie_dice_test.go
+++ zombie_dice/zombie_dice_test.go
@@ -1,34 +1,34 @@
 package zombie_dice
 
 import ( //"fmt"
-	 "testing"
+	"testing"
 
-         "github.com/Akavall/GoGamesProject/dice"
+	"github.com/Akavall/GoGamesProject/dice"
 )
 
 func TestInitializeDeck(t *testing.T) {
 	deck := initialize_deck()
 
-	expected_names := map[int]string {0 : "green",
-		1 : "green",
-		2 : "green",
-		3 : "green",
-                4 : "green",
-                5 : "green",
-		6 : "yellow",
-		7 : "yellow",
-		8 : "yellow",
-		9 : "yellow",
-		10 : "red",
-		11 : "red",
-		12 : "red"}
+	expected_names := map[int]string{0: "green",
+		1:  "green",
+		2:  "green",
+		3:  "green",
+		4:  "green",
+		5:  "green",
+		6:  "yellow",
+		7:  "yellow",
+		8:  "yellow",
+		9:  "yellow",
+		10: "red",
+		11: "red",
+		12: "red"}
 
-	for ind, d := range deck {		
-		if (d.Name != expected_names[ind]){
+	for ind, d := range deck {
+		if d.Name != expected_names[ind] {
 			t.Errorf("Expected %d: %s, but got %s", ind, expected_names[ind], d.Name)
 		}
 	}
-	
+
 }
 
 func TestShuffleDeck(t *testing.T) {
@@ -38,7 +38,7 @@ func TestShuffleDeck(t *testing.T) {
 
 	deck := []dice.Dice{green, yellow, red}
 
-        first_position := make([]string, 100)
+	first_position := make([]string, 100)
 
 	for i := 0; i < 100; i++ {
 		shuffled_deck := shuffle_deck(deck)
@@ -46,30 +46,30 @@ func TestShuffleDeck(t *testing.T) {
 		inner_yellow := 0
 		inner_red := 0
 		for ind, d := range shuffled_deck {
-			if (d.Name == "green") {
+			if d.Name == "green" {
 				inner_green++
-			} else if (d.Name == "yellow") {
+			} else if d.Name == "yellow" {
 				inner_yellow++
-			} else if (d.Name == "red") {
+			} else if d.Name == "red" {
 				inner_red++
 			}
-			if (ind == 0){
+			if ind == 0 {
 				first_position[i] = d.Name
 			}
 		}
-		if ( inner_green * inner_red * inner_yellow != 1) {
+		if inner_green*inner_red*inner_yellow != 1 {
 			t.Errorf("Got %d green, %d red, %d yellow, while expecting one of each", inner_green, inner_red, inner_yellow)
 		}
 
 	}
 
-	counter := map[string]int {"green" : 0, "red" : 0, "yellow" : 0}
+	counter := map[string]int{"green": 0, "red": 0, "yellow": 0}
 	for _, name := range first_position {
 		counter[name]++
 	}
 
 	for key, value := range counter {
-		if (value == 0) {
+		if value == 0 {
 			t.Errorf("name : %s showed up in 1st position 0 times", key)
 		}
 	}
