33732113ca76128c082fe8adcc31c0427ef1ee7a
a-temlyakov <temlyaka@gmail.com>
2015-01-09 00:49:20 -0800

    Create more generic Deck for dice

---
+++ dice/deck.go
@@ -0,0 +1,33 @@
+package dice
+
+import (
+        "math/rand"
+        "time"
+       )
+
+type Deck struct {
+    Name    string
+    Dices //to-do: refactor Dice->Die and Dices->Dice 
+}
+
+type Dices []Dice
+
+func (d Deck) Shuffle() Deck {
+    if len(d.Dices) <= 1 {
+        return d
+    }
+
+    rand.Seed(time.Now().UTC().UnixNano())
+    rand_inds := rand.Perm(len(d.Dices))
+    
+    shuffled_dice := make(Dices, len(d.Dices))
+    for i, rand_ind := range rand_inds {
+        shuffled_dice[i] = d.Dices[rand_ind] 
+    }
+
+    return Deck{Dices: shuffled_dice}
+}
+
+func (d Deck) PopDice(n int) Dices {
+    return nil
+}
